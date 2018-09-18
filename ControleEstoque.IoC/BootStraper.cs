96a44d747daf2fbcc278c31e924061344e8bb5bd
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2018-09-17 22:48:24 -0300

    Bower Components, IoC

--- ControleEstoque.IoC/BootStraper.cs
+++ ControleEstoque.IoC/BootStraper.cs
@@ -28,6 +28,7 @@ namespace ControleEstoque.IoC
 
             //Identity IoC
             container.RegisterPerWebRequest<ApplicationDbContext>();
+
             container.RegisterPerWebRequest<IUserStore<ApplicationUser>>(() => new UserStore<ApplicationUser>(new ApplicationDbContext()));
             container.RegisterPerWebRequest<IRoleStore<IdentityRole, string>>(() => new RoleStore<IdentityRole>());
             container.RegisterPerWebRequest<ApplicationRoleManager>();
