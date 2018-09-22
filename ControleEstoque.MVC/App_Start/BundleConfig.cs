785c4defc1c71974c5df8458ad11d4514d47a1cb
Rodrigo <rodrigo_cardoso97@hotmail.com>
2018-09-22 02:43:48 -0300

    DataTables buttons

--- ControleEstoque.MVC/App_Start/BundleConfig.cs
+++ ControleEstoque.MVC/App_Start/BundleConfig.cs
@@ -22,11 +22,24 @@ namespace ControleEstoque.MVC
             //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
             //          "~/Scripts/bootstrap.js"));
 
+            //AdminLTE
+            bundles.Add(new StyleBundle("~/Bundles/css")
+                .Include("~/Content/css/bootstrap.min.css", new CssRewriteUrlTransformAbsolute())
+                .Include("~/Content/DataTables-1.10.18/css/dataTables.bootstrap.min.css")
+                .Include("~/Content/Buttons-1.5.2/css/buttons.bootstrap.min.css")
+                .Include("~/Content/css/bootstrap-select.css")
+                .Include("~/Content/css/bootstrap-datepicker3.min.css")
+                .Include("~/Content/css/font-awesome.min.css", new CssRewriteUrlTransformAbsolute())
+                .Include("~/Content/css/icheck/blue.min.css", new CssRewriteUrlTransformAbsolute())
+                .Include("~/Content/css/AdminLTE.css", new CssRewriteUrlTransformAbsolute())
+                .Include("~/Content/css/skins/skin-blue.css"));
+
+            ////DataTables
+            //bundles.Add(new StyleBundle("~/Bundles/DataTables/css").Include(
+            //          "~/Content/DataTables-1.10.18/css/dataTables.bootstrap.min.css",
+            //          "~/Content/Buttons-1.5.2/css/buttons.bootstrap.min.css"));
 
             //DataTables
-            bundles.Add(new StyleBundle("~/Bundles/DataTables/css").Include(
-                      "~/Content/DataTables-1.10.18/css/dataTables.bootstrap.min.css",
-                      "~/Content/Buttons-1.5.2/css/buttons.bootstrap.min.css"));
             bundles.Add(new ScriptBundle("~/Bundles/DataTables/js").Include(
                 "~/Scripts/jquery-{version}.js",
                 "~/Content/pdfmake-0.1.36/pdfmake.min.js",
@@ -44,6 +57,7 @@ namespace ControleEstoque.MVC
             bundles.Add(new ScriptBundle("~/Bundles/js")
                 .Include("~/Scripts/jquery-{version}.js")
                 .Include("~/Content/js/plugins/bootstrap/bootstrap.js")
+                .Include("~/Scripts/bootbox.min.js")
                 .Include("~/Content/js/plugins/fastclick/fastclick.js")
                 .Include("~/Content/js/plugins/slimscroll/jquery.slimscroll.js")
                 .Include("~/Content/js/plugins/bootstrap-select/bootstrap-select.js")
@@ -55,15 +69,6 @@ namespace ControleEstoque.MVC
                 .Include("~/Content/js/adminlte.js")
                 .Include("~/Content/js/init.js"));
 
-            //AdminLTE
-            bundles.Add(new StyleBundle("~/Bundles/css")
-                .Include("~/Content/css/bootstrap.min.css", new CssRewriteUrlTransformAbsolute())
-                .Include("~/Content/css/bootstrap-select.css")
-                .Include("~/Content/css/bootstrap-datepicker3.min.css")
-                .Include("~/Content/css/font-awesome.min.css", new CssRewriteUrlTransformAbsolute())
-                .Include("~/Content/css/icheck/blue.min.css", new CssRewriteUrlTransformAbsolute())
-                .Include("~/Content/css/AdminLTE.css", new CssRewriteUrlTransformAbsolute())
-                .Include("~/Content/css/skins/skin-blue.css"));
 
 #if DEBUG
             BundleTable.EnableOptimizations = false;
--- ControleEstoque.MVC/Scripts/Cadastros/CadPais.js
+++ ControleEstoque.MVC/Scripts/Cadastros/CadPais.js
@@ -6,10 +6,22 @@ function getPaises() {
     $('#TabelaPais').DataTable({
         ajax: '/Paises/GetPaises',
         columns: [
-            { "data": "PaisId"},
+            { "data": "PaisId" },
             { "data": "Descricao" },
-            { "data": "Codigo"},
-            { "data": "Ativo" }
+            { "data": "Codigo" },
+            { "data": "Ativo" },
+            {
+                data: null,
+                className: "center",
+                width: "15%",
+                defaultContent: '<a class="btn btn-primary btn-alterar" role="button"><i class="glyphicon glyphicon-pencil"></i> Alterar</a>' +
+                                '&nbsp <a class="btn btn-danger btn-excluir" role="button"><i class="glyphicon glyphicon-trash"></i> Excluir</a>'
+            }
         ]
     });
 }
+
+//function add_anti_forgery_token(data) {
+//    data.__RequestVerificationToken = $('[name=__RequestVerificationToken]').val();
+//    return data;
+//}
