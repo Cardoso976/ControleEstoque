5c9519c23b0a25501f6350428440ad3264d8e24e
Rodrigo <rodrigo_cardoso97@hotmail.com>
2018-10-18 22:07:41 -0300

    Login Menu, favicon

---
+++ ControleEstoque.MVC/Scripts/Account/AccountMenu.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(".btn-sair").click( function () {
+        var token = $('input[name="__RequestVerificationToken"]').val();
+        $.ajax({
+            url: '/Account/LogOff',
+            type: 'POST',
+            data: {
+                __RequestVerificationToken: token
+            },
+            success: function () {
+                location.reload();
+            }
+        });
+    });
+});
