038433cd2f9a416e042e857f35ac8fff64f8ed96
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2018-12-10 23:03:31 -0200

    Relatorio posicao estoque

---
+++ ControleEstoque.MVC/Controllers/RelatorioPosicaoEstoqueController.cs
@@ -0,0 +1,34 @@
+﻿using ControleEstoque.Application.Interface;
+using System.Web.Mvc;
+using Rotativa;
+using AutoMapper;
+using System.Collections.Generic;
+using ControleEstoque.Domain.Entities;
+using ControleEstoque.MVC.ViewModels;
+
+namespace ControleEstoque.MVC.Controllers
+{
+    public class RelatorioPosicaoEstoqueController : Controller
+    {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public RelatorioPosicaoEstoqueController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+        
+        // GET: RelatorioPosicaoEstoqueView
+        public ActionResult Index()
+        {
+            var produtoViewModel = Mapper.Map<IEnumerable<Produto>, IEnumerable<ProdutoViewModel>>(_produtoAppService.GetAll());
+            return View("~/Views/Relatorio/RelatorioPosicaoEstoqueView.cshtml", produtoViewModel);
+        }
+
+        // GET: RelatorioPosicaoEstoque
+        public ActionResult ImpressaoPDF()
+        {
+            var produtoViewModel = Mapper.Map<IEnumerable<Produto>, IEnumerable<ProdutoViewModel>>(_produtoAppService.GetAll());
+            return new ViewAsPdf("~/Views/Relatorio/RelatorioPosicaoEstoque.cshtml", produtoViewModel);
+        }
+    }
+}
